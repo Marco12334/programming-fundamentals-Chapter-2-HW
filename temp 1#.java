9cd288fe8ce143a8a21e0064bf1a4fdf2fa8fdb5
Marco12334 <150499999+Marco12334@users.noreply.github.com>
2024-02-11 22:44:26 -0500

    Add files via upload

---
+++ temp 1#.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double temp_fahrenheit = 100;
+        double temp_celsius = (temp_fahrenheit - 32) * 5/9;
+        System.out.println("The temperature in Celsius is " + temp_celsius + " degrees.");
+    }
+}
